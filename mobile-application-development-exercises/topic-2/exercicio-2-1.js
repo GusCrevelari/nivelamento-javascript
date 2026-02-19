0758ffd6556e0833e537c1a6391049d40dd2e1ed
GusCrevelari <gustavocrevelari@hotmail.com>
2026-02-19 17:47:39 -0300

    exercicio 2-1 completo (declaração básica de variáveis).

---
+++ mobile-application-development-exercises/topic-2/exercicio-2-1.js
@@ -0,0 +1,8 @@
+const nomeJogador = 'Ana';
+let pontuacao = 0;
+
+console.log(nomeJogador + " tem " + pontuacao + " pontos.");
+
+pontuacao = 10;
+
+console.log(nomeJogador + " tem " + pontuacao + " pontos.");
