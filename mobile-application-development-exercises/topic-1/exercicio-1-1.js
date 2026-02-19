13187ec3118cd827b71f2ae2c480a360742cb2c6
GusCrevelari <gustavocrevelari@hotmail.com>
2026-02-19 17:40:56 -0300

    cexercicio 1-2 completo, e correção feita no exercício 1-1 (faltando ;).

---
+++ mobile-application-development-exercises/topic-1/exercicio-1-1.js
@@ -0,0 +1,3 @@
+console.log("Olá, meu nome é Gustavo Porto!");
+console.log("Atualmente, estou cursando TDS na FIAP.");
+console.log("Estou aprendendo JavaScript!");
---
+++ mobile-application-development-exercises/topic-2/exercicio-1-2.js
@@ -0,0 +1,3 @@
+console.log(100 / 4); // 25
+console.log(7 * 7); // 49
+console.log(17 % 5); // 2
