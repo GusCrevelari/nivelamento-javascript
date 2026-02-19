23960aecf4c0efab5478e65825f581ea6e94b7a3
GusCrevelari <gustavocrevelari@hotmail.com>
2026-02-19 17:54:07 -0300

    exercicio 2-2 completo (identificação de tipos de variáveis).

---
+++ mobile-application-development-exercises/topic-2/exercicio-2-2.js
@@ -0,0 +1,10 @@
+let nome = "Gustavo Porto";
+let idade = 33;
+const booleana = false;
+let indefinida;
+
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof booleana);
+console.log(typeof indefinida);
